dd434427c3703a40a1c2fb559de43c9b0814c64a
Robert Higdon <robert@ulastnight.com>
2020-08-28 15:32:23 -0700

    Fixing linting errors

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -4,7 +4,7 @@ const NameList = ({ list }) => {
   return (
     <ul className="text-left">
       {list.map(person => (
-        <li className="nameList__item">
+        <li className="nameList__item" key={`${person.location}-${person.name}-${person.age}`}>
           <span className="pr-2 relative">
             {person.name}<span className="hidden sm:inline">, {person.age}, {person.location}</span>
           </span>
--- src/views/Home.js
+++ src/views/Home.js
@@ -6,16 +6,16 @@ import Header from '../components/Header';
 import NameList from '../components/NameList';
 
 const API_URL = 'https://a6sz7rpoic.execute-api.us-west-2.amazonaws.com/dev/';
-const SUMMARY_API = 'https://covidtracking.com/api/us';
+const SUMMARY_API = 'https://api.covidtracking.com/v1/us/current.json';
 
 const Home = () => {
-  const { status, data, error } = useFetch(API_URL, 'covidNames');
-  const { status: summaryStatus, data: summaryData } = useFetch(SUMMARY_API, 'summary');
-  console.log(summaryData);
+  const { status, data } = useFetch(API_URL, 'covidNames');
+  const { data: summaryData } = useFetch(SUMMARY_API, 'summary');
+  const deaths = summaryData && summaryData[0] ? summaryData[0].death : '180,000';
   return (
     <div className="bg-dark text-light text-center">
       <div className="home__header m-auto">
-        <Header><span className="text-cta">128,000</span> people have lost their lives to <span className="whitespace-no-wrap">COVID-19</span>.<br />These are their names.</Header>
+        <Header><span className="text-cta">{deaths.toLocaleString()}</span> Americans have lost their lives to <span className="whitespace-no-wrap">COVID-19</span>.<br />These are their names.</Header>
         <h2 className="text-xl leading-6">Their memories live forever.</h2>
         <p className="leading-tight my-12">COVID-19 is caused by a coronavirus called SARS-CoV-2. Older adults and people who have severe underlying medical conditions like heart or lung disease or diabetes seem to be at higher risk for developing more serious complications from COVID-19 illness.</p>
       </div>
